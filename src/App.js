7c456cf3062f13d8a3a624675c528659027f1f68
RukhanaAliyeva96 <rukhana.aliyeva96@gmail.com>
2024-12-22 13:04:31 +0200

    js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
+import "./App.css";
+import TicTacToe from "./components/TicTacToe";
 
-// Importing the CSS styles for the App component
-import './App.css';
-// Importing the TicTacToe component
-import TicTacToe from './components/TicTacToe';
-
-// Rendering the TicTacToe component inside the App component
 function App() {
   return <TicTacToe />;
 }
 
-// Exporting the App component to be used in other parts of the application
 export default App;
